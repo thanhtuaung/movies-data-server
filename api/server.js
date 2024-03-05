ff800dbabae37a14d2190f4190afe54514cd86b5
Than Htoo Aung <116297911+thanhtuaung@users.noreply.github.com>
2024-03-06 00:46:48 +0630

    rewrite route

--- api/server.js
+++ api/server.js
@@ -19,8 +19,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/Tbl_MovieList/:id': '/Tbl_MovieList?MovieId=:id',
 }))
 server.use(router)
 server.listen(3000, () => {
